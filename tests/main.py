daa1f4159a47e5b6dd31ab5d8debf93b4a58f58d
Egil <egil.moller@freecode.no>
2020-09-19 16:11:27 +0200

    More tests

--- tests/main.py
+++ tests/main.py
@@ -2,10 +2,11 @@ import unittest
 
 import eop
 import pandas as pd
+import numpy as np
 
 class TestDataInstance(unittest.TestCase):
-    test_data_a = pd.DataFrame({"foo": [1, 2, 3], "bar": [4, 5, 6]})
-    test_data_b =  pd.DataFrame({"fie": [7, 8, 9]})
+    test_data_a = pd.DataFrame({"x": [1, 2, 3], "y": [4, 5, 6], "z": [7, 8, 9]})
+    test_data_b =  pd.DataFrame({"doi": [77, 88, 99]})
     
     def test_sub(self):
         sub = eop.DataInstance(self.test_data_a.copy())
@@ -17,13 +18,13 @@ class TestDataInstance(unittest.TestCase):
         sub = eop.DataInstance(self.test_data_a.copy())
         main = eop.DataInstance(self.test_data_b.copy())
         main["sub"] = sub
-        self.assertEqual(main.loc[[0, 2]]["sub"]["foo"].iloc[1], 3)
+        self.assertEqual(main.loc[[0, 2]]["sub"]["x"].iloc[1], 3)
 
     def test_sub_col(self):
         sub = eop.DataInstance(self.test_data_a.copy())
         main = eop.DataInstance(self.test_data_b.copy())
         main["sub"] = sub
-        self.assertEqual(main[("sub", "foo")].loc[0], 1)
+        self.assertEqual(main[("sub", "x")].loc[0], 1)
 
     def test_sub_type(self):
         sub = eop.B(self.test_data_a.copy())
@@ -31,6 +32,13 @@ class TestDataInstance(unittest.TestCase):
         main["sub"] = sub
         self.assertIsInstance(main, eop.A)
         self.assertIsInstance(main["sub"], eop.B)
+        
+    def test_dtypes(self):
+        sub = eop.Point3d(self.test_data_a.copy())
+        main = eop.X(self.test_data_b.copy())
+        main["sub"] = sub
+        self.assertEqual(main.df.dtypes[("doi", np.NaN)], np.dtype("int64"))
+        self.assertEqual(main["sub"].df.dtypes[("x",)], np.dtype("float64"))
 
 if __name__ == '__main__':
     unittest.main()
