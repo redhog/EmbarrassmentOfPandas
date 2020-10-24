94170115553a1bc187d0843cddc472892163d3c1
Egil <egil.moller@freecode.no>
2020-10-24 17:07:55 +0200

    More initialization work

--- eop/__init__.py
+++ eop/__init__.py
@@ -88,13 +88,10 @@ class Instance(object):
     DTypes = None
     
     def __new__(cls, data = None, extension = None, meta=None, filter = None):
-        self = object.__new__(cls)
         if isinstance(data, Container):
-            object.__setattr__(self, "data", data)
-        else:
-            object.__setattr__(self, "data", Container(data, extension, meta))
-        object.__setattr__(self, "filter", filter if filter is not None else Filter())
+            return cls.new_from_container(data, filter)
 
+        self = cls.new_from_data(data, extension, meta, filter)
         if cls.DTypes is None:
             return self
 
@@ -103,7 +100,37 @@ class Instance(object):
         self2.data.meta = meta
         
         return self2
-    
+
+    @classmethod
+    def new_from_data(cls, data = None, extension = None, meta=None, filter = None):
+        self = object.__new__(cls)
+        object.__setattr__(self, "filter", filter if filter is not None else Filter())
+        if isinstance(data, dict):
+            base = pd.DataFrame({key:value for key, value in data.items() if isinstance(value, list)})
+            def instancify(key, value):
+                if isinstance(value, Instance):
+                    return value
+                return cls.DTypes[key](value)
+            extension = pd.DataFrame({key:[instancify(key, value)] for key, value in data.items() if isinstance(value, (Instance, dict))}, index=[0])
+            basecols = list(base.columns)
+            extcols = list(extension.columns)
+            for key in extcols:
+                base[key] = np.bool_(False)
+            for key in basecols:
+                extension[key] = np.bool_(False)
+            object.__setattr__(self, "data", Container(base, extension, meta))
+        else:
+            object.__setattr__(self, "data", Container(data, extension, meta))
+        return self
+            
+    @classmethod
+    def new_from_container(cls, data, filter=None):
+        self = object.__new__(cls)
+        object.__setattr__(self, "filter", filter if filter is not None else Filter())
+        if isinstance(data, Container):
+            object.__setattr__(self, "data", data)
+        return self
+        
     @classmethod
     def new_empty(cls):
         base = None
@@ -170,12 +197,15 @@ class Instance(object):
                 pd.DataFrame({col:[self.unselect(row=True)]}))
         ).select(self.filter.reset(col=True))
     
-    def assign(self, other):
+    def assign(self, other, prefix=()):
+        def log(*arg):
+            print(("  " * len(prefix)) + " ".join(str(item) for item in arg))
+        log(".".join(str(item) for item in prefix) + ":")
         if self.filter.is_extractable:
             col = self.filter.col[-1]
-            print("Extract", col)
+            log("Extract", col)
             self.unselect(col=True).assign(
-                other.wrap(col))
+                other.wrap(col), prefix=prefix + ("Wrapped",))
         else:
             rows, cols = self.filtered
             other_rows, other_cols = other.filtered
@@ -183,7 +213,7 @@ class Instance(object):
             col_pos = self.filter.col_position
 
             if col_pos is not None or not self.filter.col:
-                print("Include all columns")
+                log("Include all columns")
                 for new_col in set(other_cols) - set(self.data.base.columns):
                     if other.data.extension.loc[0, new_col] is np.bool_(False):
                         self.data.base[new_col] = np.NaN
@@ -200,14 +230,14 @@ class Instance(object):
             other_extension = other.data.extension[other_cols]
 
             if len(cols) and self.filter.col:
-                print("Copy columns by position", cols)
+                log("Copy columns by position", cols)
                 # FIXME: Handle replacing columns
                 assert len(cols) == len(other_cols), "Both instances must have the same number of columns"
                 renames = dict(zip(other_cols, cols))
                 other_base = other_base.rename(columns=renames)
                 other_extension = other_extension.rename(columns=renames)
             else:
-                print("Update or columns", cols)
+                log("Update or columns", cols)
                 cols = other_cols
                 
             def insert(df1, df2, pos):
@@ -215,16 +245,16 @@ class Instance(object):
                 return df1.iloc[:idx].append(df2).append(df1.iloc[idx:])
 
             if row_pos is not None:
-                print("Insert rows at", row_pos)
+                log("Insert rows at", row_pos)
                 self.data.base = insert(self.data.base, other_base, row_pos)
             elif len(rows) == len(other_base):
-                print("Update rows", len(rows), cols)
+                log("Update rows", len(rows), cols)
                 self.data.base.loc[rows, cols] = other_base
             elif self.filter.row:
-                print("Replace rows", len(rows), len(other_base))
+                log("Replace rows", len(rows), len(other_base))
                 self.data.base = insert(self.data.base.drop(rows), other_base, next(iter(rows)))
             else:
-                print("Merge rows", len(rows), len(other_base))
+                log("Merge rows", len(rows), len(other_base))
                 existing = self.data.base.index & other_base.index
                 new = (self.data.base.index ^ other_base.index) & other_base.index                
                 self.data.base.loc[existing, cols] = other_base.loc[existing]
@@ -233,8 +263,8 @@ class Instance(object):
             for col in other_extension.columns:
                 if col not in self.data.extension.columns: continue
                 if not self.data.is_extension_col(col): continue
-                print("Assign extension column", col)
-                self.data.extension.loc[0, col].select(self.filter.reset(col=True)).assign(other_extension.loc[0, col])
+                log("Assign extension column", col)
+                self.data.extension.loc[0, col].select(self.filter.reset(col=True)).assign(other_extension.loc[0, col], prefix=prefix+(col,))
 
     def flatten(self, prefix = (), include_types=False, include_first_filter=False):
         if include_types:
