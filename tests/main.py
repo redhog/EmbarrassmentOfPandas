17e2ab3e987ea3a245a67bfa062c5b8fd776e7f5
Egil <egil.moller@freecode.no>
2020-11-07 22:42:54 +0100

    Some cleanup

--- tests/main.py
+++ tests/main.py
@@ -13,87 +13,89 @@ class Point3D(eop.DataInstance):
     DTypes = {"x": np.dtype("float64"),
               "y": np.dtype("float64"),
               "z": np.dtype("float64")}
-
     def summary(self):
         return eop.DataInstance(pd.DataFrame({"summary": (["/".join(str(item) for item in row) for idx, row in self.df.iterrows()])}))
             
+
 class X(eop.DataInstance):
     DTypes = {"doi": np.dtype("int64")}
+
 
 class Y(eop.DataInstance):
     DTypes = {"doi": np.dtype("int64"), "sub": Point3D}
 
 
+test_data_a = pd.DataFrame({"x": [1, 2, 3], "y": [4, 5, 6], "z": [7, 8, 9]})
+test_data_b =  pd.DataFrame({"doi": [77, 88, 99]})
+test_data_c =  pd.DataFrame({"nana": [2, 3, 5]})
+
 class TestDataInstance(unittest.TestCase):
-    test_data_a = pd.DataFrame({"x": [1, 2, 3], "y": [4, 5, 6], "z": [7, 8, 9]})
-    test_data_b =  pd.DataFrame({"doi": [77, 88, 99]})
-    test_data_c =  pd.DataFrame({"nana": [2, 3, 5]})
     
     def test_sub(self):
-        sub = eop.DataInstance(self.test_data_a.copy())
-        main = eop.DataInstance(self.test_data_b.copy())
+        sub = eop.DataInstance(test_data_a.copy())
+        main = eop.DataInstance(test_data_b.copy())
         main["sub"] = sub
         self.assertIsInstance(main["sub"], eop.DataInstance)
         
     def test_sub_loc(self):
-        sub = eop.DataInstance(self.test_data_a.copy())
-        main = eop.DataInstance(self.test_data_b.copy())
+        sub = eop.DataInstance(test_data_a.copy())
+        main = eop.DataInstance(test_data_b.copy())
         main["sub"] = sub
         self.assertEqual(main.loc[[0, 2]]["sub"]["x"].iloc[1], 3)
 
     def test_sub_type(self):
-        sub = B(self.test_data_a.copy())
-        main = A(self.test_data_b.copy())
+        sub = B(test_data_a.copy())
+        main = A(test_data_b.copy())
         main["sub"] = sub
         self.assertIsInstance(main, A)
         self.assertIsInstance(main["sub"], B)
         
     def test_method_type(self):
-        sub = B(self.test_data_a.copy())
-        main = A(self.test_data_b.copy())
+        sub = B(test_data_a.copy())
+        main = A(test_data_b.copy())
         main["sub"] = sub
         self.assertIsInstance(main.head(), A)
         self.assertIsInstance(main.head()["sub"], B)
         
     def test_dtypes(self):
-        sub = Point3D(self.test_data_a.copy())
-        main = X(self.test_data_b.copy())
+        sub = Point3D(test_data_a.copy())
+        main = X(test_data_b.copy())
         main["sub"] = sub
         self.assertEqual(main.dtypes["doi"], np.dtype("int64"))
         self.assertEqual(main["sub"].dtypes["x"], np.dtype("float64"))
 
     @unittest.skip("Not yet implemented")
     def test_summary(self):
-        sub = Point3D(self.test_data_a.copy())
-        main = X(self.test_data_b.copy())
+        sub = Point3D(test_data_a.copy())
+        main = X(test_data_b.copy())
         main["sub"] = sub
         self.assertEqual(main.summary()[("sub", "[Point3D]", "summary")].loc[0], "1.0/4.0/7.0")
 
     @unittest.skip("Not yet implemented")
     def test_summary_single_col(self):
-        main = eop.DataInstance(self.test_data_a.copy())
-        sub = X(self.test_data_b.copy())
+        main = eop.DataInstance(test_data_a.copy())
+        sub = X(test_data_b.copy())
         main["sub"] = sub
         self.assertEqual(main.summary()[("sub", "[X]", "doi", "[int64]")].loc[0], 77)
 
     def test_sub_sub(self):
-        main = eop.DataInstance(self.test_data_b.copy())
-        sub = A(self.test_data_a.copy())
-        sub2 = B(self.test_data_c.copy())
+        main = eop.DataInstance(test_data_b.copy())
+        sub = A(test_data_a.copy())
+        sub2 = B(test_data_c.copy())
         main["sub"] = sub
         main["sub"]["xxx"] = sub2
         self.assertIsInstance(main["sub"]["xxx"], B)
 
     def test_attributes(self):
-        sub = eop.DataInstance(self.test_data_a.copy())
-        main = eop.DataInstance(self.test_data_b.copy())
+        sub = eop.DataInstance(test_data_a.copy())
+        main = eop.DataInstance(test_data_b.copy())
         sub.gazonk = 47
         main["sub"] = sub
         self.assertEqual(main["sub"].gazonk, 47)
 
     def test_flatten(self):
-        sub = Point3D(self.test_data_a.copy())
-        main = Y(self.test_data_b.copy())
+        sub = Point3D(test_data_a.copy())
+        main = Y(test_data_b.copy())
         main["sub"] = sub
 
         unflattened = Y(main.flatten())
